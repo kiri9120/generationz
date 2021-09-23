b540424d35368155215534ffba58a7ea033eb3ed
Masato Kiryu <kiri9120@gmail.com>
2021-09-23 22:57:30 +0900

    create slashed headline

--- front-page.php
+++ front-page.php
@@ -26,13 +26,29 @@ $container = get_theme_mod( 'understrap_container_type' );
     <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
         <main class="site-main" id="main">
-            <section class="text-center py-5">
-                <h2 class="h2 mb-4">メッセージが入ります</h2>
-                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                </p>
+            <section class="py-5">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-8">
+                        <div class="slashed-wrapper text-center mb-5">
+                            <h2 class="slashed">
+                                <div class="top" title="MISSION"></div>
+                                <div class="bot" title="MISSION"></div>
+                            </h2>
+                        </div>
+                        <p class="display-4 lh-xl font-weight-bold">
+                            テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります
+                        </p>
+                    </div>
+                </div>
             </section>
             <section class="py-5">
-                <h2 class="h2 text-center mb-5">Partner</h2>
+                <!-- <h2 class="h2 text-center mb-5">Partner</h2> -->
+                <div class="slashed-wrapper text-center mb-5">
+                    <h2 class="slashed">
+                        <div class="top" title="FOUNDER"></div>
+                        <div class="bot" title="FOUNDER"></div>
+                    </h2>
+                </div>
                 <div class="row justify-content-between text-center">
                     <div class="col-6 col-md-4 col-lg-3 pb-4">
                         <img src="https://placehold.jp/200x200.png" alt="">
