a5c82f8d3208402e72e9d130d9e381a583c5dc75
Masato Kiryu <kiri9120@gmail.com>
2021-10-17 15:08:15 +0900

    add smooth scroll

--- js/child-theme.js
+++ js/child-theme.js
@@ -9978,6 +9978,21 @@
     }
   })();
 
+  // Add your custom JS here.
+  // ページ内リンク スムーススクロール
+  jQuery(document).ready(function () {
+    jQuery('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      var target = this.hash;
+      var $target = jQuery(target);
+      jQuery('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+  });
+
   exports.Alert = Alert;
   exports.Button = Button;
   exports.Carousel = Carousel;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,17 @@
 // Add your custom JS here.
+
+// ページ内リンク スムーススクロール
+jQuery(document).ready(function(){
+    jQuery('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = jQuery(target);
+
+        jQuery('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+        window.location.hash = target;
+        });
+    });
+});
