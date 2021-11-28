2aec18d5c0ed326a9dbbf978fd699434066cf20e
Masato Kiryu <kiri9120@gmail.com>
2021-11-29 08:03:29 +0900

    update header design

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
     <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
         <main class="site-main" id="main">
-            <section id="mission" class="py-5">
+            <section id="about" class="py-5">
                 <div class="row justify-content-center">
                     <div class="col-12 col-md-10 col-lg-8">
                         <div class="slashed-wrapper text-center mb-5">
@@ -36,11 +36,12 @@ $container = get_theme_mod( 'understrap_container_type' );
                             </h2>
                         </div>
                         <p class="display-4 lh-xl font-weight-bold">
-                        （こんな感じのテキスト入れたい）私たちは、サラダを売るだけの外食企業ではありません。届けていくのは、CRISPらしいレストラン体験です。創業当時は、とにかく素敵なお客様と仲間に囲まれた良いお店をつくりたい、という想いを込めてCRISP SALAD WORKS1号店を開業しました。その想いは変わりませんが、今では私たちのやりたいことはもっともっと⼤きく、外⾷のビジネスモデルを180度ひっくり返すような「新しい外⾷」を⽬指しています。私たちは、飲食にテクノロジーの力を取り入れて、オンラインでもオフラインでも、あらゆる場所で人と人とのリアルなつながりが感じられるレストラン体験をつくっていきます。自分たちが信じるものに手を抜かずに、 仲間に、商品に、お店に、そしてお客様に愛情をもって本気で向き合いながら。日本の外食のあり方を変え、世の中を変える。私たちCRISPは、共に働くパートナーとお店を愛するファンと一緒に、飲食の未来をもっと面白くしていきます。</p>
+                            自分らしく生きたことはあるか。僕たちが創るのはコミュニティじゃない、次世代だ。<br>「Z世代」と呼ばれる、1990年代中盤から2000年代終盤までに生まれた僕たち世代に向けて、革命の鐘を。<br>個々の鐘が集まり、音楽となり、人の心を動かしていく。
+                        </p>
                     </div>
                 </div>
             </section>
-            <section id="founder" class="py-5">
+            <section id="members" class="py-5">
                 <!-- <h2 class="h2 text-center mb-5">Partner</h2> -->
                 <div class="slashed-wrapper text-center mb-5">
                     <h2 class="slashed">
@@ -51,8 +52,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                 <div class="founder-list row justify-content-between text-center">
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/kent1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/kent2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/kent1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/kent2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">僕と私と株式会社 CEO</p>
@@ -61,8 +64,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hinako1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hinako2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hinako1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hinako2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">イチミ株式会社 CEO</p>
@@ -71,8 +76,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hayato1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hayato2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hayato1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hayato2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社ANCR CEO</p>
@@ -81,8 +88,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/keita1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/keita2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/keita1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/keita2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社ness CEO</p>
@@ -91,8 +100,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ryohei1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ryohei2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ryohei1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ryohei2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社NOPAIN CEO</p>
@@ -101,8 +112,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ayano1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ayano2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ayano1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ayano2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社YOICHO CEO</p>
@@ -111,8 +124,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/takuya1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/takuya2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/takuya1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/takuya2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社Next Paradigm CEO</p>
@@ -121,8 +136,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hiroko1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hiroko2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hiroko1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hiroko2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">株式会社TAWO CEO</p>
@@ -131,8 +148,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tsubasa1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tsubasa2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tsubasa1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tsubasa2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">Being BaSE株式会社 CEO</p>
@@ -141,8 +160,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                     <div class="col-6 col-md-4 col-lg-3 mb-4">
                         <div class="flipped-profile">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/masato1.jpg" class="front" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/masato2.jpg" class="flipped" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/masato1.jpg" class="front"
+                                alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/masato2.jpg" class="flipped"
+                                alt="">
                         </div>
                         <div class="text-center my-3">
                             <p class="text-sm mb-2">conf合同会社 CEO</p>
